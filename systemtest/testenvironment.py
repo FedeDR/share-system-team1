d1db12110ad2721cd7c5d74747b2466fd815a63a
Federico Della Rovere <dellarovere.federico@gmail.com>
2014-09-09 15:18:06 +0200

    add method for add user daemon istance

--- systemtest/testenvironment.py
+++ systemtest/testenvironment.py
@@ -5,6 +5,33 @@ import os
 import json
 import time
 import shutil
+#import check password server function
+from ...server.server import PasswordChecker as password_checker
+
+def check_password(password):
+    '''
+    password checker adapter
+    parameters:
+        password - string
+
+    return a Boolean
+    '''
+    if password_checker(password) == password:
+        return True
+    else:
+        return False
+
+
+def check_username(username):
+    '''
+    username checker adapter
+    parameters:
+        username - string
+
+    retunr a Boolean
+    '''
+    email_regex = re.compile('[^@]+@[^@]+\.[^@]+')
+    return email_regex.match(username)
 
 
 class EnvironmentManager(object):
@@ -78,3 +105,42 @@ class EnvironmentManager(object):
             }
             json.dump(to_save, datafile)
 
+    def add_dmn_istance(self, ist_id=None, credential=None, svr_rec=False, dmn_rec=False):
+        '''
+        add an istance at the daemon istance list
+        parameters:
+            id - identifier of istance
+                default - None, the id will be automatically assigned
+            credential - dict contain compatible username ("usr" key) and compatible password ("psw" key) of istance
+                exemple {"usr": "user@test.com", "psw": "Password<3"}
+                if svr_rec and dmn_rec are both False this var will be ignore
+                default - None, create a void istance
+            svr_rec - Boolean, mean the istance is registered on server yet.
+                default - False
+            dmn_rec - Boolean, mean the istance is logged in yet
+                can be True only if svr_rec are True
+                default - False
+        '''
+        if not ist_id:
+            condition = True
+            while condition:
+                ist_id = ''.join(['ist_', self.inc_id])
+                if ist_id not in self.dmn_istance_list:
+                    condition = False
+                self.inc_id += 1
+        else:
+            if ist_id in self.dmn_istance_list:
+                raise
+
+        self.dmn_istance_list[ist_id] = {
+            'svr_rec': svr_rec,
+            'dmn_rec': dmn_rec,
+        }
+
+        if svr_rec or dmn_rec:
+            if check_password(credential['psw']) and check_username(credential['usr']):
+                self.dmn_istance_list[ist_id]['usr'] = credential['usr']
+                self.dmn_istance_list[ist_id]['psw'] = credential['psw']
+            else:
+                self.dmn_istance_list[ist_id]['usr'] = None
+                self.dmn_istance_list[ist_id]['psw'] = None
